db5a3ca4cd0f8c8f1e61328a830f227142b82427
sgeorge-nb <122819133+sgeorge-nb@users.noreply.github.com>
2026-02-15 11:11:37 +0100

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { RouterView } from 'vue-router'
 import { useAuthStore } from './stores/authStore'
 import { useProgressStore } from './stores/progressStore'
