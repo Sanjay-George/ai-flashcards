71e985b67513dca0af900fdddb08ba83a39bfa95
sgeorge-nb <122819133+sgeorge-nb@users.noreply.github.com>
2026-04-11 09:52:34 +0200

    Add vite devtools

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 
 export default defineConfig(({ mode }) => {
     // Load env file from frontend directory
     const env = loadEnv(mode, path.resolve(__dirname, '.'), '')
 
     return {
-        plugins: [vue()],
+        plugins: [vue(), vueDevTools()],
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, './src')
